785d003c361d7a7c546bde8bf7b7ea2d9de0a2ff
Sararat Burke <sararatburke@Sararats-MacBook-Pro.local>
2015-12-29 16:31:12 -0500

    made slides image on index page work by adding code on js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var main = function() {
 		$('.portfolio-menu').toggle();
 	  });
 
-	$('.nextArrow').click(function(){
+	/*$('.nextArrow').click(function(){
 		var currentSlide = $('.active-slide');
     	var nextSlide = currentSlide.next();
 
@@ -26,9 +26,46 @@ var main = function() {
 
 		currentSlide.fadeOut(600).removeClass('.active-slide');
 		previousSlide.fadeIn(600).addClass('.active-slide');
-	});
+	});*/
 
 }	
 
 $(document).ready(main);
 
+images = ['index/image/CA02.jpg', 'index/image/CA01.jpg', 'index/image/FW01.jpg', 'index/image/Animal01.jpg']
+
+
+setInterval(forwardImage, 4000);
+
+
+//This function will find the key for the current Image
+function currentImageKey() {
+    i = $.inArray($('.slide').attr('src'), images);
+    return i;
+}
+
+
+//This function will move the slideshow forward one
+function forwardImage() {
+    currentImageKey();
+    if (i < images.length - 1) {
+        changeImage(i + 1);
+    } else {
+        changeImage(0);
+    }
+}
+
+
+//This function will change to image to whatever the variable i passes to it
+function changeImage(i) {
+    $('.slide').stop().animate({
+        opacity: 100,
+    }, 200, function() {
+        $('.slide').attr('src', images[i]);
+        $('.slideHolder').load(function() {
+            $('.slide').stop().animate({
+                opacity: 1,
+            }, 200)
+        })
+    })
+}
