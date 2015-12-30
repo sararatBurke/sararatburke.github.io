13b2ecf5ab49f6a00d0ce018a167234361f4e910
Sararat Burke <sararatburke@Sararats-MacBook-Pro.local>
2015-12-29 19:00:19 -0500

    deleted comments in js

--- index.js
+++ index.js
@@ -12,30 +12,6 @@ var dropdown = function() {
 		$('.portfolio-menu').toggle();
 	  });
 
-	/*$('.nextArrow').click(function(){
-		var currentSlide = $('.active-slide');
-    	var nextSlide = currentSlide.next();
-
-    	if(nextSlide.length === 0) {
-      		nextSlide = $('.slide').first();
-    	}
-
-    	currentSlide.fadeOut(600).removeClass('.active-slide');
-    	nextSlide.fadeIn(600).addClass('.active-slide');
-	});
-
-	$('.previousArrow').click(function(){
-		var currentSlide = $('.active-slide');
-		var previousSlide currentSlide.previous();
-
-		if(previousSlide.length === 0){
-			previousSlide = $('.slide').last();
-		}
-
-		currentSlide.fadeOut(600).removeClass('.active-slide');
-		previousSlide.fadeIn(600).addClass('.active-slide');
-	});*/
-
 }	
 
 $(document).ready(dropdown);
